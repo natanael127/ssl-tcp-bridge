6382d2f0ae9fd188c62970fb18fddae902a9593a
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2020-06-09 21:52:46 -0300

    Creates an independent blocking-socket server

--- ssl_tcp_bridge.py
+++ ssl_tcp_bridge.py
@@ -2,16 +2,24 @@
 import socket
 
 # CONSTANT DEFINITIONS ------------------------------------------------------------------------------- #
+SOCKET_TUPLE_INDEX_ADDR = 0
+SOCKET_TUPLE_INDEX_PORT = 1
 SOCKET_BUFFER_SIZE = 65536
+MAX_CONNECTIONS = 7
 
 # USER VARIABLES ------------------------------------------------------------------------------------- #
-TargetServer = ('192.168.0.10', 49823) #TODO: receive parameters from command line
+TargetServer = ('192.168.0.10', 50010) #TODO: receive parameters from command line
+LocalServer = ('localhost',30000)
 
 # OBJECTS -------------------------------------------------------------------------------------------- #
-ConnToServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ConnToServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #TODO: Stablish many connections
+LocalHost = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#TODO: work on IPv6
 
 # ALGORITHM ------------------------------------------------------------------------------------------ #
-print('Connecting to ' + TargetServer[0] + ':' + str(TargetServer[1]));
+
+#To target server
+print('Connecting to ' + TargetServer[SOCKET_TUPLE_INDEX_ADDR] + ':' + str(TargetServer[SOCKET_TUPLE_INDEX_PORT]))
 if ConnToServer.connect_ex(TargetServer) != 0:
 	print('Connection error')
 else:
@@ -33,3 +41,26 @@ else:
 
 	print('Closing socket')
 	ConnToServer.close()
+
+#From interested client
+LocalHost.bind(LocalServer)
+LocalHost.listen(MAX_CONNECTIONS)
+print('Waiting for a connection to the local sever')
+ConnInterestedClient, InterestedClientAddress = LocalHost.accept()
+try:
+	print ('Connection from ' + InterestedClientAddress[SOCKET_TUPLE_INDEX_ADDR] + ':' + str(InterestedClientAddress[SOCKET_TUPLE_INDEX_PORT]))
+
+	# Receive the data in small chunks and retransmit it
+	while True:
+		data = ConnInterestedClient.recv(SOCKET_BUFFER_SIZE)
+		print('Received ' + str(len(data)) + ' bytes: ' + data.decode("utf-8"))
+		if data:
+			print ('Sending data back to the client')
+			ConnInterestedClient.send(data)
+		else:
+			print('No more data from client')
+			break
+		
+finally:
+	# Clean up the connection
+	ConnInterestedClient.close()
